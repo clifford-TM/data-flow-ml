f4bae1af0ba8b7d363c026864ed5983adc3d3546
clifford-TM <fdavid.hl3@gmail.com>
2025-05-04 17:08:11 -0300

    Refatoração de execução

--- db_utils.py
+++ db_utils.py
@@ -25,8 +25,10 @@ elif db_env == "local":
     )
     
 
-# Cursos para executar comandos sql
+# Cursor para executar comandos sql
 cursor = db.cursor()
+
+
 
 
 # Função para executar comandos SQL sem perder a conexão (necessária pelo ambiente azure)
@@ -41,12 +43,60 @@ def executar_sql(query, valores=None, fetch=False):
         else:
             cursor.execute(query)
 
+        
+
         # Executa a query SQL e retorna o resultado. Se `fetch` for "all", retorna todas as linhas; caso contrário, retorna a próxima linha. 
         # Se `fetch` for False ou None, não retorna nada.
         if fetch:
             return cursor.fetchall() if fetch == "all" else cursor.fetchone()
+        else:
+            # Se não for select fazemos um commit
+            db.commit()
         
     # tratamento de erro
+    except mysql.connector.Error as err:
+        print(f"[ERRO MySQL] {err}")
+        db.rollback()
+        raise
+
+# Definindo lotes para execução de múltiplos comandos
+lote = []
+batch_size = 10
+
+def adicionar_batch(query, valores):
+    global batch
+    batch.append((query, valores))
+    
+    if len(batch) >= batch_size:
+        executar_batch()
+
+# Função para executar todos os comandos em batch
+def executar_batch():
+    global batch
+    try:
+        db.ping(reconnect=True)
+        for query, valores in batch:
+            if valores:
+                cursor.execute(query, valores)
+            else:
+                cursor.execute(query)
+                
+        db.commit()
+        batch = []
+    except mysql.connector.Error as err:
+        print(f"[ERRO MySQL - batch] {err}")
+        db.rollback()
+
+
+# Execução imediata para ações criticas
+def execucao_imediata(query, valores=None, fetch=False):
+    try:
+        db.ping(reconnect=True)
+        cursor.execute(query, valores)
+        db.commit()
+
+        if fetch:
+            return cursor.fetchall() if fetch == "all" else cursor.fetchone()
     except mysql.connector.Error as err:
         print(f"[ERRO MySQL] {err}")
         raise
