ad21bef340a59517b1480b3a9daf87bbe0f03c40
clifford-TM <fdavid.hl3@gmail.com>
2025-05-06 21:24:45 -0300

    Adição de canalizações no formato da operação

--- canalizacoes.py
+++ canalizacoes.py
@@ -3,42 +3,122 @@ import datetime
 canalizacoes = {
     "SMG1": {
         "horarios": [datetime.time(9, 0), datetime.time(17, 0)],
-        "rampas": [101, 102],
+        "rampas": [101, 202],
     },
-    "SRJ2": {
-        "horarios": [datetime.time(12, 0), datetime.time(14, 0)],
-        "rampas": [103, 104],
+    "SPR2": {
+        "horarios": [datetime.time(12, 0), datetime.time(20, 0)],
+        "rampas": [103, 204],
     },
-    "SES3": {
-        "horarios": [datetime.time(13, 0), datetime.time(20, 0)],
-        "rampas": [105, 106],
+    "SRJ3": {
+        "horarios": [datetime.time(13, 0), datetime.time(22, 0)],
+        "rampas": [105, 206],
     },
-    "SSP4": {
-        "horarios": [datetime.time(9, 0), datetime.time(15, 0), datetime.time(22, 0)],
-        "rampas": [107, 108],
+    "SRS4": {
+        "horarios": [datetime.time(14, 0), datetime.time(17, 0)],
+        "rampas": [107, 208],
     },
-    "SMG5": {
-        "horarios": [datetime.time(8, 0), datetime.time(16, 0)],
-        "rampas": [109, 110],
+    "SSP5": {
+        "horarios": [datetime.time(9, 0), datetime.time(20, 0)],
+        "rampas": [109, 210],
     },
-    "SPR6": {
-        "horarios": [datetime.time(10, 0), datetime.time(18, 0)],
-        "rampas": [111, 112],
-    },
-    "SRS7": {
+    "SBA6": {
         "horarios": [datetime.time(12, 0), datetime.time(19, 0)],
-        "rampas": [113, 114],
+        "rampas": [111, 212],
     },
-    "SSP8": {
-        "horarios": [datetime.time(11, 0), datetime.time(20, 0)],
-        "rampas": [115, 116],
+    "SCE7": {
+        "horarios": [datetime.time(13, 0), datetime.time(17, 0)],
+        "rampas": [113, 214],
+    },
+    "SAL8": {
+        "horarios": [datetime.time(14, 0), datetime.time(22, 0)],
+        "rampas": [115, 216],
     },
     "SMG9": {
-        "horarios": [datetime.time(7, 0), datetime.time(16, 0)],
-        "rampas": [117, 118],
+        "horarios": [datetime.time(9, 0), datetime.time(19, 0)],
+        "rampas": [117, 218],
     },
-    "SRJ10": {
+    "SPR10": {
+        "horarios": [datetime.time(12, 0), datetime.time(17, 0)],
+        "rampas": [119, 220],
+    },
+    "SRJ11": {
+        "horarios": [datetime.time(13, 0), datetime.time(20, 0)],
+        "rampas": [121, 222],
+    },
+    "SRS12": {
+        "horarios": [datetime.time(14, 0), datetime.time(22, 0)],
+        "rampas": [123, 224],
+    },
+    "SSP13": {
+        "horarios": [datetime.time(9, 0), datetime.time(19, 0)],
+        "rampas": [125, 226],
+    },
+    "SBA14": {
+        "horarios": [datetime.time(12, 0), datetime.time(20, 0)],
+        "rampas": [127, 228],
+    },
+    "SCE15": {
+        "horarios": [datetime.time(13, 0), datetime.time(17, 0)],
+        "rampas": [129, 230],
+    },
+    "SAL16": {
+        "horarios": [datetime.time(14, 0), datetime.time(22, 0)],
+        "rampas": [131, 232],
+    },
+    "SMG17": {
         "horarios": [datetime.time(9, 0), datetime.time(17, 0)],
-        "rampas": [119, 120],
+        "rampas": [133, 234],
+    },
+    "SPR18": {
+        "horarios": [datetime.time(0, 0), datetime.time(3, 0)],
+        "rampas": [135, 236],
+    },
+    "SRJ19": {
+        "horarios": [datetime.time(1, 0), datetime.time(4, 0)],
+        "rampas": [137, 238],
+    },
+    "SRS20": {
+        "horarios": [datetime.time(2, 0), datetime.time(3, 0)],
+        "rampas": [139, 240],
+    },
+    "SSP21": {
+        "horarios": [datetime.time(0, 0), datetime.time(2, 0)],
+        "rampas": [141, 242],
+    },
+    "SBA22": {
+        "horarios": [datetime.time(1, 0), datetime.time(3, 0)],
+        "rampas": [143, 244],
+    },
+    "SCE23": {
+        "horarios": [datetime.time(4, 0), datetime.time(2, 0)],
+        "rampas": [145, 246],
+    },
+    "SAL24": {
+        "horarios": [datetime.time(0, 0), datetime.time(1, 0)],
+        "rampas": [147, 248],
+    },
+    "SMG25": {
+        "horarios": [datetime.time(3, 0), datetime.time(4, 0)],
+        "rampas": [149, 250],
+    },
+    "SPR26": {
+        "horarios": [datetime.time(13, 0), datetime.time(22, 0)],
+        "rampas": [151, 252],
+    },
+    "SRJ27": {
+        "horarios": [datetime.time(9, 0), datetime.time(14, 0)],
+        "rampas": [153, 254],
+    },
+    "SRS28": {
+        "horarios": [datetime.time(12, 0), datetime.time(20, 0)],
+        "rampas": [155, 256],
+    },
+    "SSP29": {
+        "horarios": [datetime.time(14, 0), datetime.time(17, 0)],
+        "rampas": [157, 258],
+    },
+    "SBA30": {
+        "horarios": [datetime.time(17, 0), datetime.time(22, 0)],
+        "rampas": [159, 260],
     },
 }
